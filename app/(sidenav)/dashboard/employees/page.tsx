7ca69dbd010393604ff0dbe58fff5d0f7bb2b477
Matias Andrada <matiasandrada5622@gmail.com>
2025-01-28 03:44:23 -0300

    Refactor employee dashboard to include TableEmployees component and add EmployeeSchedule model

--- app/(sidenav)/dashboard/employees/page.tsx
+++ app/(sidenav)/dashboard/employees/page.tsx
@@ -1,6 +1,12 @@
+import { TableEmployees } from "@/components/dashboard/employees/tableEmployees";
 //TODO 1: Table for view employees and their permissions
 //TODO 2: Table for view schedules and edit them
-
+//TODO 3: Implement TUI calendar to display schedules
 export default function Page() {
-  return <h1>Employees</h1>;
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Personal</h1>
+      <TableEmployees />
+    </section>
+  );
 }
---
+++ components/dashboard/employees/tableEmployees.tsx
@@ -0,0 +1,193 @@
+"use client";
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/table";
+import { User } from "@nextui-org/user";
+import { Chip } from "@nextui-org/chip";
+import { Tooltip } from "@nextui-org/tooltip";
+import { EyeIcon, DeleteIcon, EditIcon } from "@/components/ui/icons/icons";
+
+interface UserInterface {
+  id: number;
+  name: string;
+  role: string;
+  team: string;
+  status: string;
+  age: string;
+  avatar: string;
+  email: string;
+}
+
+export const columns = [
+  { name: "NAME", uid: "name" },
+  { name: "ROLE", uid: "role" },
+  { name: "STATUS", uid: "status" },
+  { name: "ACTIONS", uid: "actions" },
+];
+
+export const users: UserInterface[] = [
+  {
+    id: 1,
+    name: "Tony Reichert",
+    role: "CEO",
+    team: "Management",
+    status: "active",
+    age: "29",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+    email: "tony.reichert@example.com",
+  },
+  {
+    id: 2,
+    name: "Zoey Lang",
+    role: "Technical Lead",
+    team: "Development",
+    status: "paused",
+    age: "25",
+    avatar: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
+    email: "zoey.lang@example.com",
+  },
+  {
+    id: 3,
+    name: "Jane Fisher",
+    role: "Senior Developer",
+    team: "Development",
+    status: "active",
+    age: "22",
+    avatar: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+    email: "jane.fisher@example.com",
+  },
+  {
+    id: 4,
+    name: "William Howard",
+    role: "Community Manager",
+    team: "Marketing",
+    status: "vacation",
+    age: "28",
+    avatar: "https://i.pravatar.cc/150?u=a048581f4e29026701d",
+    email: "william.howard@example.com",
+  },
+  {
+    id: 5,
+    name: "Kristen Copper",
+    role: "Sales Manager",
+    team: "Sales",
+    status: "active",
+    age: "24",
+    avatar: "https://i.pravatar.cc/150?u=a092581d4ef9026700d",
+    email: "kristen.cooper@example.com",
+  },
+];
+
+const statusColorMap: { [key: string]: string } = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
+
+export function TableEmployees() {
+  const renderCell = React.useCallback(
+    (user: UserInterface, columnKey: string) => {
+      const cellValue = user[columnKey as keyof UserInterface];
+
+      switch (columnKey) {
+        case "name":
+          return (
+            <User
+              avatarProps={{ radius: "lg", src: user.avatar }}
+              description={user.email}
+              name={cellValue as string}
+            >
+              {user.email}
+            </User>
+          );
+        case "role":
+          return (
+            <div className="flex flex-col">
+              <p className="text-bold text-sm capitalize">
+                {cellValue as string}
+              </p>
+              <p className="text-bold text-sm capitalize text-default-400">
+                {user.team}
+              </p>
+            </div>
+          );
+        case "status":
+          return (
+            <Chip
+              className="capitalize"
+              color={
+                statusColorMap[user.status] as
+                  | "success"
+                  | "danger"
+                  | "warning"
+                  | "default"
+                  | "primary"
+                  | "secondary"
+                  | undefined
+              }
+              size="sm"
+              variant="flat"
+            >
+              {cellValue as string}
+            </Chip>
+          );
+        case "actions":
+          return (
+            <div className="relative flex items-center gap-2">
+              <Tooltip content="Detalles">
+                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                  <EyeIcon />
+                </span>
+              </Tooltip>
+              <Tooltip content="Editar usuario">
+                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                  <EditIcon />
+                </span>
+              </Tooltip>
+              <Tooltip color="danger" content="Eliminar usuario">
+                <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                  <DeleteIcon />
+                </span>
+              </Tooltip>
+            </div>
+          );
+        default:
+          return cellValue as string;
+      }
+    },
+    []
+  );
+
+  return (
+    <>
+      <h2>Empleados</h2>
+      <Table aria-label="Example table with custom cells">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody items={users}>
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey.toString())}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
